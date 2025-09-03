458abbe0009a0f7d365db2f81427af91301653cb
Thiago <thiagooalessandro@gmail.com>
2025-09-03 02:49:56 -0300

    FIX: Alterando nome de pastas

--- backend/SuperHero.Application/obj/Debug/netcoreapp3.1/SuperHeroi.Application.AssemblyInfo.cs
+++ backend/SuperHero.Application/obj/Debug/netcoreapp3.1/SuperHeroi.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperHeroi.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aa256fa031d285c1546a28964fa5a4f6fb5931b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperHeroi.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperHeroi.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
